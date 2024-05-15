5b3decbb795a88d1ad9d9b4008cf126a6344cc12
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-05-14 23:23:05 -0700

    Add project files.

---
+++ CraftableShipSystemUpgrader/BepInPlugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using ResourceAssets;
+
+namespace CraftableShipSystemUpgrader
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Void Crew.exe")]
+    [BepInDependency("VoidManager")]
+    public class BepinPlugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            Log = Logger;
+            GUIDUnion guid = new GUIDUnion("49991ea1005c0134cb8e8febcd0dfe8b");
+            CraftingRules cr = VoidManager.Content.Craftables.Instance.GetRecipe(guid);
+            cr.CraftingMethod = CraftMethod.Resource;
+            cr.Creation = new ResourceCurrency(CG.Ship.Object.ResourceType.Alloys, 24);
+
+            VoidManager.Content.Craftables.Instance.SetRecipe(guid, MyPluginInfo.PLUGIN_GUID, cr);
+
+            VoidManager.Content.Unlocks.Instance.SetUnlockOptions(guid, MyPluginInfo.PLUGIN_GUID, new CG.Client.UserData.UnlockOptions() { RankRequirement = 30, UnlockCriteria = CG.Client.UserData.UnlockCriteriaType.PlayerRank});
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ CraftableShipSystemUpgrader/VoidManagerPlugin.cs
@@ -0,0 +1,13 @@
+﻿using VoidManager.MPModChecks;
+
+namespace CraftableShipSystemUpgrader
+{
+    public class VoidManagerPlugin : VoidManager.VoidPlugin
+    {
+        public override MultiplayerType MPType => MultiplayerType.All;
+
+        public override string Author => "Dragon";
+
+        public override string Description => "Makes ship system upgraders craftable. All must have.";
+    }
+}
